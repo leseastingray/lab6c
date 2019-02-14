3011e54dfb5369af463d2bcce5b2a1530e199f89
Harrison-Lane-1/22 <ycazam@my.lanecc.edu>
2019-02-13 16:22:09 -0800

    Lab4Part2

---
+++ CardMaintenance/CardClassLibrary/Card.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace CardClassLibrary
+{
+    public class Card
+    {
+        // Card class variables
+        private int suit; // suit ranges from 1 to 4
+        private int value; // value ranges from 1 to 13
+
+        private string[] suits = { "????", "clubs", "diamonds", "hearts", "spades" };
+        private string[] values = { "????", "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
+
+        // Card class constructors
+        // Default constructor
+        public Card()
+        {
+            value = 1;
+            suit = 1;
+        }
+
+        // Overloaded constructor
+        public Card(int v, int s)
+        {
+            value = v;
+            suit = s;
+        }
+
+        // Getters and Setters
+        public int Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public int Suit
+        {
+            get
+            {
+                return suit;
+            }
+            set
+            {
+                suit = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return values[value] + " of " + suits[suit];
+        }
+
+        // CardIsSuit Methods
+        public bool IsHeart()
+        {
+            if (suit == 3)
+                return true;
+            else
+                return false;
+        }
+
+        // CardIsFace Methods
+        public bool IsFaceCard()
+        {
+            if (value >= 11 && value <= 13)
+                return true;
+            else
+                return false;
+        }
+
+        public bool IsQueen()
+        {
+            if (value == 12)
+                return true;
+            else
+                return false;
+        }
+
+        // CardIsColor Methods
+        public bool IsBlack()
+        {
+            if (suit == 1 || suit == 4)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ CardMaintenance/CardTests/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardClassLibrary;
+
+
+namespace CardTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCardConstructors();
+
+            Card aS = new Card(1, 4);
+            Card qH = new Card(12, 3);
+
+            TestCardIsMethods(aS);
+            TestCardIsMethods(qH);
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+
+        // Test Card Constructors
+        static void TestCardConstructors()
+        {
+            Card card1 = new Card();
+            Card card2 = new Card(12, 3);
+
+            Console.WriteLine("Testing Constructors");
+            Console.WriteLine("Default constructor. Expecting default values. " + card1.ToString() + ".");
+            Console.WriteLine("Overloaded constructor. Expecting queen of hearts. " + card2.ToString() + ".");
+            Console.WriteLine();
+        }
+
+        // Test Card ToString Method
+        static void TestCardToString(Card card)
+        {
+            Console.WriteLine("Testing toString Method");
+            Console.WriteLine("The card is " + card.ToString() + ".");
+            Console.WriteLine();
+        }
+
+        // Test Card IsMethods
+        static void TestCardIsMethods(Card card)
+        {
+            Console.WriteLine("Testing Is Methods");
+            Console.WriteLine("The card is " + card.ToString() + ".");
+            Console.WriteLine();
+
+            if (card.IsHeart())
+                Console.WriteLine("The card is a heart.");
+            else
+                Console.WriteLine("The card is not a heart.");
+            Console.WriteLine();
+
+            if (card.IsFaceCard())
+                Console.WriteLine("The card is a face card");
+            else
+                Console.WriteLine("The card is not a face card.");
+            Console.WriteLine("");
+
+            if (card.IsQueen())
+                Console.WriteLine("The card is a queen.");
+            else
+                Console.WriteLine("The card is not a queen.");
+            Console.WriteLine();
+
+            if (card.IsBlack())
+                Console.WriteLine("The card is black.");
+            else
+                Console.WriteLine("The card is not black.");
+
+
+        }
+    }
+}
