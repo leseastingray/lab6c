b692a7ed3c5f75169976cdfbb7711659174edbba
Harrison-Lane-1/22 <ycazam@my.lanecc.edu>
2019-03-06 14:43:49 -0800

    first edit

---
+++ CardMaintenance/CardClassLibrary/CardDeck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardClassLibrary
+{
+    public class CardDeck
+    {
+        private List<Card> cards;
+
+        public CardDeck()
+        {
+            cards = new List<Card>();
+
+        }
+    }
+}
