05d3573e37fa997800410ddc148d7a67f583ee49
Student <Student@1913503-C18102.lanecc.edu>
2019-03-14 16:47:16 -0700

    working on card deck tests

--- CardMaintenance/CardClassLibrary/CardDeck.cs
+++ CardMaintenance/CardClassLibrary/CardDeck.cs
@@ -13,6 +13,15 @@ namespace CardClassLibrary
         public CardDeck()
         {
             cards = new List<Card>();
+            for (int i = 1; i <= 13; i++)
+            {
+                int value = i;
+                for (int j = 1; j <= 4; j++)
+                {
+                    int suit = j;
+                    cards.Add(new Card(i, j));
+                }
+            }
 
         }
 
@@ -31,25 +40,33 @@ namespace CardClassLibrary
         {
             Random generator = new Random();
 
-            for (int i = 1; i <= 20; i++)
+            for (int i = 0; i < cards.Count; i++)
             {
-                int randomIndex = generator.Next(1, 21);
+                int randomIndex = generator.Next(1, cards.Count);
                 Card temp = cards[i];
                 cards[i] = cards[randomIndex];
                 cards[randomIndex] = temp;
             }
         }
         // DealCard
+        // Get index 0 from list
         // Remove index 0 from list
         // Return index 0 card
         public Card DealCard()
         {
+            Card dealtCard = cards[0];
 
+            cards.Remove(dealtCard);
+            return dealtCard;
         }
 
-        public void IsEmpty()
+        public bool IsEmpty()
         {
-
+            if (cards.Count == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public override string ToString()
---
+++ CardMaintenance/CardDeckTests/CardDeckTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardClassLibrary;
+
+namespace CardDeckTests
+{
+    class CardDeckTest
+    {
+        static void Main(string[] args)
+        {
+            // TestCardDeckAll();
+
+            TestCardDeckConstructor();
+            TestCardDeckShuffle();
+            // TestCardDeckDeal()
+            // TestCardDeckIsEmpty()
+            // TestCardDeckToString();
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static void TestCardDeckConstructor()
+        {
+            CardDeck cDeck = new CardDeck();
+
+            Console.WriteLine("Testing constructor");
+            Console.WriteLine("Default constructor. Expecting count of 52 " + cDeck.NumCards);
+            Console.WriteLine("Default constructor. Expecting list of 52 cards \n" + cDeck);
+        }
+
+        static void TestCardDeckShuffle()
+        {
+            CardDeck cDeck = new CardDeck();
+            cDeck.ShuffleCards();
+
+            Console.WriteLine("Testing shuffle method");
+
+            Console.WriteLine("Shuffle method. Expecting shuffled list of 52 cards \n" + cDeck);
+        }
+    }
+}
