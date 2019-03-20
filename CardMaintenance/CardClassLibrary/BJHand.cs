d515241ce66e495173a4799765695df634711f25
Amy Lese <amylese@Amys-MacBook-Pro.local>
2019-03-20 15:48:26 -0700

    added lab 7 extra credit classes

---
+++ CardMaintenance/CardClassLibrary/BJHand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CardClassLibrary
+{
+    public class BJHand : Hand
+    {
+        // fields
+        int score = 0;
+        protected List<Card> bJHandCards;
+
+        // BJHand constructor
+        public BJHand()
+        {
+           bJHandCards = new List<Card>();
+        }
+
+        // score property
+        public int Score
+        {
+            get
+            {
+                foreach (Card cardInBJH in bJHandCards)
+                {
+                    score += cardInBJH.Value;
+                }
+                return score;
+            }
+        }
+        // bool methods
+        public bool HasAce(int value)
+        {
+            foreach (Card cardInBJH in handCards)
+            {
+                if (cardInBJH.Value == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool IsBusted(int value1, int value2)
+        {
+            if (bJHandCards.Score > 21 )
+        }
+    }
+}
---
+++ CardMaintenance/CardClassLibrary/Hand.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CardClassLibrary
+{
+    public class Hand
+    {
+        // field variable
+        protected List<Card> handCards;
+
+        // constructor
+        public Hand()
+        {
+            handCards = new List<Card>();
+        }
+        // count property
+        public int NumCards
+        {
+            get
+            {
+                return handCards.Count;
+            }
+        }
+        // add method
+        public void Add(Card card)
+        {
+            handCards.Add(card);
+        }
+        // discard method
+        public void Discard(Card card)
+        {
+            handCards.Remove(card);
+        }
+        // indexer
+        public Card this[int i]
+        {
+            get
+            {
+                return handCards[i];
+            }
+            set
+            {
+                handCards[i] = value;
+            }
+        }
+
+        // find specific card by value only
+        public Card IndexOf(int value)
+        {
+            foreach (Card cardInH in handCards)
+            {
+                if (cardInH.Value == value)
+                {
+                    return cardInH;
+                }
+            }
+            return null;
+        }
+        // find specific card by value and suit
+        public Card IndexOf(int value, int suit)
+        {
+            foreach (Card cardInH in handCards)
+            {
+                if (cardInH.Value == value)
+                {
+                    if (cardInH.Suit == suit)
+                        return cardInH;
+                }
+                return null;
+            }
+            return null;
+        }
+
+        // bool methods
+        public bool HasCard(Card card)
+        {
+            // go through handCards list
+            // check each card
+            // if true
+            // return true + break
+            foreach (Card cardInH in handCards)
+            {
+                if (cardInH == card)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool HasCard(int value)
+        {
+            foreach (Card cardInH in handCards)
+            {
+                if (cardInH.Value == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool HasCard(int value, int suit)
+        {
+            foreach (Card cardinH in handCards)
+            {
+                if (cardinH.Value == value && cardinH.Suit == suit)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        // ToString method
+        public override string ToString()
+        {
+            string output = "";
+            foreach (Card cardInH in handCards)
+            {
+                output += cardInH.ToString() + "\n";
+            }
+            return output;
+        }
+    }
+}
